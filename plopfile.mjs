2abc5c143c2ca9a7942cdbdad6880103b3b4b20e
Edwards, Alexander <Alexander.Edwards@GEHA.com>
2022-12-02 11:30:14 -0600

    added additional plop handlers, and updated Parcel to ignore the babel config.

--- plopfile.mjs
+++ plopfile.mjs
@@ -12,20 +12,36 @@ export default function (plop) {
     actions: [
       {
         type: 'add',
-        path: 'src/components/{{name}}/index.tsx',
+        path: 'src/components/{{pascalCase name}}/index.tsx',
         templateFile: '.plop/component.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: '.plop/component-test.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/styles/components/{{pascalCase name}}/_{{camelCase name}}.tsx',
+        templateFile: '.plop/component-style.hbs',
       },
       {
         type: 'modify',
         path: 'src/components/index.tsx',
         pattern: /(\/\/ PLOP)/g,
-        template: 'export { {{name}} } from "./{{name}}";\n$1',
+        template: 'export { {{pascalCase name}} } from "./{{pascalCase name}}";\n$1',
+      },
+      {
+        type: 'modify',
+        path: 'src/styles/components/_components.scss',
+        pattern: /(\/\/ PLOP)/g,
+        template: '@import "./{{pascalCase name}}/{{camelCase name}}";\n$1',
       },
       {
         type: 'modify',
         path: 'src/index.tsx',
         pattern: / }/g,
-        template: ', {{name}} }',
+        template: ', {{pascalCase name}} }',
       },
     ],
   });
